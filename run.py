2047863233cf285a1f5207e0c99d7adcdb9dc8bc
yelan187 <3230106014@zju.edu.cn>
2025-03-20 18:46:24 +0800

    change io to ws

--- run.py
+++ run.py
@@ -5,15 +5,15 @@ from src.bot import Bot
 from src.event import MessageEvent
 
 async def main(bot: Bot):
-    await bot.io.connect()
+    await bot.ws.connect()
 
     if bot.ws.role == 'client':
         while True:
-            res = await bot.io.recv()
+            res = await bot.ws.recv()
             if res:
                 message_list: str = bot.handle_message(res)
                 async for message in message_list:
-                    await bot.io.send(message)
+                    await bot.ws.send(message)
 
 if __name__ == '__main__':
     ws = WS(host='127.0.0.1', port=3001, role='client', callback=MessageEvent())
